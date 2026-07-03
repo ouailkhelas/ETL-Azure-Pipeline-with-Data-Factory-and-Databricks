941d47d7dbec24b5a452d2d34cb0e60eb3c7e2a2
Ouail Mokhtar Khelas <ouail.khelas@univ-constantine2.dz>
2026-07-03 21:35:19 +0100

    v2

--- cloudshell/deploy.sh
+++ cloudshell/deploy.sh
@@ -1,16 +1,13 @@
-
-
+# deploy IAC files
 # Navigate to terraform directory
 cd terraform/
 
 echo ""
 echo "Step 1: Terraform Init"
 terraform init
-
 echo ""
 echo "Step 2: Terraform Plan"
 terraform plan -out=tfplan
-
 echo ""
 echo "Step 3: Terraform Apply"
 terraform apply tfplan
